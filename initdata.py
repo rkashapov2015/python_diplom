542be17acdf204ad6907c5bf675232c8183fec82
Rinat Kashapov <rinatkzz@yandex.ru>
2018-10-08 00:05:20 +0300

    add readme

--- initdata.py
+++ initdata.py
@@ -2,5 +2,4 @@ APP_ID = 6703198
 OAUTH_URL = 'https://oauth.vk.com/authorize'
 API_URL = 'https://api.vk.com/method/'
 VK_URL = 'https://vk.com'
-#TOKEN = '3c69b79707168581f82a40801ca955ee8d5512d7393f05a93514b2fe99f73e6b8a6db46c3605791097ad7'
 TOKEN = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
